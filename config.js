d2b28964bfc8fd6995404e83bdf0ffd63adf3b1b
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-21 22:08:11 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+const assert = require('assert');
+
+dotenv.config();
+
+const {
+    PORT,
+    HOST,
+    HOST_URL,
+
+    API_KEY,
+    AUTH_DOMAIN,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID
+} = process.env;
+
+assert(PORT, 'PORT is required');
+assert(HOST, 'HOST is required');
+
+module.exports = {
+    port: PORT,
+    host, HOST,
+    host_url: HOST_URL,
+
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID
+}
