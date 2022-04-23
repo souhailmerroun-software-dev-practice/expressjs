f2d4eac1f2089f7264e33e6c5160aaa5936c972a
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-23 14:44:16 +0200

    WIP debugging

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,14 +1,21 @@
 const db = require('../db');
 const taskModel = require('../models/task');
+const { collection, addDoc } = require('firebase/firestore');
 
 const storeTask = async (req, res, next) => {
-    try {
-        const data = req.body;
-        const task = await db.collection('tasks').doc().set(data);
-        res.send(task);
+    console.log(req.body);
+    res.send("wait");
+    /*try {
+        const { title } = req.body;
+        const docRef = await addDoc(collection(db, "tasks"), {
+            title: title
+        });
+
+        res.send(docRef);
     } catch (error) {
-        res.status(400).send(error.message);
-    }
+        console.log(error);
+        res.send(error);
+    }*/
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
 const cors = require('cors');
 app.use(cors());
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
 
 const tasksRoutes = require('./routes/tasksRoutes');
 app.use('/api', tasksRoutes.routes);
